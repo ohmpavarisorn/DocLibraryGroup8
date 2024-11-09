c9caa8ad55d2d8c1887a85237267ae65cc735b70
titapabum <titapa.bum@spumail.net>
2024-11-09 21:59:39 +0700

    Update PermissionManage.jsx

--- src/page/PermissionManage/PermissionManage.jsx
+++ src/page/PermissionManage/PermissionManage.jsx
@@ -1,1 +1,22 @@
-import './PermissionManage.css'
+import "./PermissionManage.css";
+import React, { useState } from "react";
+
+function PermissionManage() {
+    const [name, setName] = useState('');
+  return (
+    <div>
+      <h1>จัดการสิทธิ์ในการเข้าถึง</h1>
+      <form>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="กรอกรหัสบุคลากร หรือ ชื่อบุคลากร"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default PermissionManage;
